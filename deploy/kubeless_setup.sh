88d920ba3987824cf986afbeb877ca5ff252914b
Torma Kristóf <tormakristof@tormakristof.eu>
2019-03-11 17:14:21 +0100

    Update kubeless_setup.sh

--- deploy/kubeless_setup.sh
+++ deploy/kubeless_setup.sh
@@ -3,3 +3,9 @@ RELEASE=$(curl -s https://api.github.com/repos/kubeless/kubeless/releases/latest
 kubectl create ns kubeless
 kubectl create -f https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless-$RELEASE.yaml
 #kubectl create -f https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless-non-rbac-$RELEASE.yaml
+
+#kubeless command
+OS=$(uname -s| tr '[:upper:]' '[:lower:]')
+curl -OL https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless_$OS-amd64.zip && \
+  unzip kubeless_$OS-amd64.zip && \
+  sudo mv bundles/kubeless_$OS-amd64/kubeless /usr/local/bin/
