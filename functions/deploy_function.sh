277b3e2b41e56fc84ec7cee2ab822dee64ab210e
Torma Kristóf <tormakristof@tormakristof.eu>
2019-04-02 23:49:08 +0200

    Update deploy_function.sh

--- functions/deploy_function.sh
+++ functions/deploy_function.sh
@@ -2,7 +2,7 @@
 #$1=runtime
 #$2=filename
 #$3=function name
-#$4/handler
+#$4=handler
 
 kubeless function deploy $3 --runtime $1 --from-file $2 --handler $4
 kubeless trigger http create $3 --function-name $3 --path $3 --hostname $3.kubeless
