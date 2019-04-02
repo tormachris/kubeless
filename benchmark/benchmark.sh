06189c2a203caf63fd85cb515d17471bf3e8ddb0
Torma Kristóf <tormakristof@tormakristof.eu>
2019-04-03 00:22:42 +0200

    Update benchmark.sh

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -55,7 +55,8 @@ do
     echo -e "Output of $function is:\n"
     curl --data-binary "@$function.body.txt" --header "Host: $function.kubeless" --header "Content-Type:application/json" http://$kuberhost/$function
     echo -e "\n"
-    tr -d '\n' < $function.body.txt
+    tr -d '\n' < $function.body.txt > $function.body.txt
+    tr -d '\r' < $function.body.txt > $function.body.txt
     for connection in "${connections[@]}"
     do
         if [[ $connection -lt 41 ]]
