1447c964ec6ee5e6a1bdf0cdb8cf766deef570f3
tormakris <tormakristof@tormakristof.eu>
2019-04-02 23:45:31 +0200

    bunch of changes

--- functions/deploy_function.sh
+++ functions/deploy_function.sh
@@ -2,9 +2,9 @@
 #$1=runtime
 #$2=filename
 #$3=function name
+#$4/handler
 
-FILENAME=cut $2 -d"." -c2
-kubeless function deploy $3 --runtime $1 --from-file $2 --handler $FILENAME.$3
-kubeless trigger http create $3--function-name $3 --path $3 --hostname $3.kubeless
+kubeless function deploy $3 --runtime $1 --from-file $2 --handler $4
+kubeless trigger http create $3 --function-name $3 --path $3 --hostname $3.kubeless
 
 #Test with curl --data '{"Another": "Echo"}' --header "Host: get-python.192.168.99.100.nip.io" --header "Content-Type:application/json" 192.168.99.100/echo
