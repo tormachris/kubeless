99086153730e609638591d1356da9bb536e3f109
Torma Kristóf <tormakristof@tormakristof.eu>
2019-04-24 13:27:05 +0200

    Update cilium_setup.sh

--- deploy/cilium_setup.sh
+++ deploy/cilium_setup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-## Initialize Kubernetes
-kubeadm init --ignore-preflight-errors=SystemVerification
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-
 ## Apply Cilium CNI plugin
 kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.4/examples/kubernetes/1.13/cilium.yaml
