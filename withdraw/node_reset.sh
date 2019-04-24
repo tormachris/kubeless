f4d971f3e28922f4354e3daaf62090cb8f889f56
Torma Kristóf <tormakristof@tormakristof.eu>
2019-04-24 13:29:03 +0200

    Update node_reset.sh

--- withdraw/node_reset.sh
+++ withdraw/node_reset.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubeadm reset --force
+test -f /etc/default/kubelet && rm /etc/default/kubelet
 iptables -F && iptables -t nat -F && iptables -t mangle -F && \
 iptables -X && iptables -t nat -X && iptables -t mangle -X
-service docker restart
+systemctl restart docker
