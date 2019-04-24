f13a5d9ab5caa6008f9b26f33546d402bdeb6416
Torma Kristóf <tormakristof@tormakristof.eu>
2019-04-24 13:27:58 +0200

    Update weavenet_setup.sh

--- deploy/weavenet_setup.sh
+++ deploy/weavenet_setup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-## Initialize Kubernetes
-kubeadm init --ignore-preflight-errors=SystemVerification
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-
 ## Apply WeaveNet CNI plugin
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
