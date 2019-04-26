5b4e6a3341e89ffc710bbb819829554bb8e1f656
Torma Kristóf <tormakristof@tormakristof.eu>
2019-04-26 15:25:05 +0200

    Update deploy_function.sh

--- functions/deploy_function.sh
+++ functions/deploy_function.sh
@@ -5,7 +5,7 @@
 #$4=handler
 
 kubeless function deploy $3 --runtime $1 --from-file $2 --handler $4 --namespace kubeless --trigger-http
-#kubeless trigger http create $3 --function-name $3 --path $3 --hostname $3.kubeless
+kubeless trigger http create $3 --function-name $3 --path $3 --hostname $3.kubeless
 kubeless autoscale create $3 --max 32 --metric "cpu" --min 3 --value "50"
 
 #Test with curl --data '{"Another": "Echo"}' --header "Host: get-python.192.168.99.100.nip.io" --header "Content-Type:application/json" 192.168.99.100/echo
